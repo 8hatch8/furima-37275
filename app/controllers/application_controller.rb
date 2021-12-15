1d1ce6a87427a174d314dbd5544a87d6b3f4859a
8hatch8 <yutaka.pg@gmail.com>
2021-12-15 11:08:25 +0900

    Add new registration func

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :basic_auth 一時停止
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   private
@@ -9,4 +10,17 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+      keys:[:email,
+            :password,
+            :nickname,
+            :family_name,
+            :first_name,
+            :family_name_kana,
+            :first_name_kana,
+            :birthday])
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, :family_name, :first_name, :family_name_kana,
+            :first_name_kana, :birthday, presence: true
+  # パスワードは半角英数字混合必須
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }, allow_blank: true	
+  # 名前は全角漢字・ひらがな・カタカナいずれか
+  validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }, allow_blank: true
+  # カナは全角カタカナのみ
+  validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, allow_blank: true
+
 end
