7a20980416d1eb41a2b02a9112d353629d83c97d
8hatch8 <yutaka.pg@gmail.com>
2021-12-15 16:36:00 +0900

    Fix by rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,13 @@
 class User < ApplicationRecord
-  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :nickname, :family_name, :first_name, :family_name_kana,
             :first_name_kana, :birthday, presence: true
   # パスワードは半角英数字混合必須
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
-    message: "英字と数字の両方を含めて設定してください" }, allow_blank: true	
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '英字と数字の両方を含めて設定してください' }, allow_blank: true
   # 名前は全角漢字・ひらがな・カタカナいずれか
-  validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
-    message: "全角文字を使用してください" }, allow_blank: true
+  validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }, allow_blank: true
   # カナは全角カタカナのみ
-  validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/,
-    message: "全角カタカナを使用してください" }, allow_blank: true
-
+  validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' }, allow_blank: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    email                   {Faker::Internet.free_email}
-    password                {Faker::Lorem.characters(number: 6, min_alpha:1, min_numeric:1)}
+    email                   { Faker::Internet.free_email }
+    password                { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation   { password }
-    nickname                {Faker::Internet.username}
-    family_name             {Gimei.last.kanji}
-    first_name              {Gimei.first.kanji}
-    family_name_kana        {Gimei.last.katakana}
-    first_name_kana         {Gimei.first.katakana}
-    birthday                {Faker::Date.birthday(min_age: 6, max_age: 80)}
+    nickname                { Faker::Internet.username }
+    family_name             { Gimei.last.kanji }
+    first_name              { Gimei.first.kanji }
+    family_name_kana        { Gimei.last.katakana }
+    first_name_kana         { Gimei.first.katakana }
+    birthday                { Faker::Date.birthday(min_age: 6, max_age: 80) }
   end
 end
