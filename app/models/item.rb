eebcb54a6b118b7683cfa528d871fe9daf29314e
8hatch8 <yutaka.pg@gmail.com>
2021-12-16 02:37:17 +0900

    Set associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage_payer
+  belongs_to :prefecture
+  belongs_to :Preparation_days
+
+  belongs_to :user
 end
