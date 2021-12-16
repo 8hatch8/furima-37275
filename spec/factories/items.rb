f9666463b3945efe52a9955d070e7a62b1fb746e
8hatch8 <yutaka.pg@gmail.com>
2021-12-16 16:14:58 +0900

    Set item unit test

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    name                { Faker::Commerce.product_name }
+    text                { Faker::Lorem.sentence }
+    price               { Faker::Number.between(from: 300, to: 9999999) }
+    category_id         { Faker::Number.between(from: 1,   to: 10) }
+    condition_id        { Faker::Number.between(from: 1,   to: 6) }
+    postage_payer_id    { Faker::Number.between(from: 1,   to: 2) }
+    prefecture_id       { Faker::Number.between(from: 1,   to: 47) }
+    preparation_days_id { Faker::Number.between(from: 1,   to: 3) }
     
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open("app/assets/images/flag.png"), filename: "flag.png")
+    end
   end
 end
