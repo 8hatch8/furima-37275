9181196b62de5302dbe267b2f970532753b67ef4
8hatch8 <yutaka.pg@gmail.com>
2021-12-19 01:07:53 +0900

    Add action contracts#create

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,8 +1,25 @@
 class ContractsController < ApplicationController
 
-  def index
+  def new
     @item = Item.find(params[:item_id])
-    # @contract_address = Contract_address.new 
+    @contract_address = ContractAddress.new 
+  end
+
+  def create
+    @contract_address = ContractAddress.new(contract_params)
+    if @contract_address.valid?
+      @contract_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :new
+    end
+  end
+
+  private
+
+  def contract_params
+    params.require(:contract_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building, :tel, :user_id).merge(item_id: params[:item_id], user_id: current_user.id)
   end
 
 end
